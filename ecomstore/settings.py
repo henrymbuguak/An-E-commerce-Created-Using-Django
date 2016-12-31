2ae834842e887725b0cb6f57b65800ab69470f27
henrymbuguak <henrymbuguak@gmail.com>
2016-12-31 20:23:24 +0300

    Database connection in setting.py configured

--- ecomstore/settings.py
+++ ecomstore/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'ecomstore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',#'django.db.backends.sqlite3',
+        'NAME': 'ecomstore',#os.path.join(BASE_DIR, 'db.sqlite3'),
+        'USER': 'username',
+        'PASSWORD': 'password',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
